c0f152955efa4ada5e7e3b06e10567e1cab9541f
Marcus Mosley <marcus.mosley1993@gmail.com>
2024-12-19 17:36:17 -0600

    room_size.rb

--- room_size.rb
+++ room_size.rb
@@ -1,10 +1,22 @@
-puts "Enter the length of the room in meters:"
+#puts "Enter the length of the room in meters:"
+#length = gets.chomp.to_f
+#puts "Enter the width of the room in meters:"
+#width = gets.chomp.to_f
+
+#total = (length * width).round(2)
+#feet = (total * 10.7639).round(2)
+
+
+#puts "The area of the room is #{total} square meters (#{feet} square feet)."
+
+puts "Enter the length of the room in feet:"
 length = gets.chomp.to_f
-puts "Enter the width of the room in meters:"
+puts "Enter the width of the room in feet:"
 width = gets.chomp.to_f
 
-total = (length * width).round(2)
-feet = (total * 10.7639).round(2)
+sqfeet = (length * width).round(2)
+sqinches = (sqfeet * 12).round(2)
+sqcentimeters = (sqinches * 2.54).round(2)
 
 
-puts "The area of the room is #{total} square meters (#{feet} square feet)."
+puts "The area of the room is #{sqfeet} square feet (#{sqinches} square inches)(#{sqcentimeters} square centimeters)."
