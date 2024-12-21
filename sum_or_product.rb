e2a82d087bdac4013262e3830dabb301962edd97
Marcus Mosley <marcus.mosley1993@gmail.com>
2024-12-21 12:21:32 -0600

    sum_or_product.rb

--- sum_or_product.rb
+++ sum_or_product.rb
@@ -1,14 +1,14 @@
-def compute_sum(number)
-  total = 0
-  1.upto(number) { |value| total += value }
-  total
-end
+#def compute_sum(number)
+  #total = 0
+  #1.upto(number) { |value| total += value }
+  #total
+#end
 
-def compute_product(number)
-  total = 1
-  1.upto(number) { |value| total *= value }
-  total
-end
+#def compute_product(number)
+  #total = 1
+  #1.upto(number) { |value| total *= value }
+  #total
+#end
 
 puts "Please enter an integer greater than 0:"
 number = gets.chomp.to_i
@@ -16,10 +16,10 @@ puts "Enter 's' to compute the sum, 'p' to compute the product."
 operation = gets.chomp
 
 if operation == "s"
-  sum = compute_sum(number)
+  sum = (0..number).inject(:+)
   puts "The sum of integers between 1 and #{number} is #{sum}."
 elsif operation == 'p'
-  product = compute_product(number)
+  product = (1..number).inject(:*)
   puts "The product of integer between 1 and #{number} is #{product}."
 else
   puts "Oops. Unknown operation."
