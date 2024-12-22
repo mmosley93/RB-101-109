d4bb757b91abf145424845ba0dbc9a2f447652b9
Marcus Mosley <marcus.mosley1993@gmail.com>
2024-12-22 16:17:15 -0600

    exclusive.rb

--- exclusive_or.rb
+++ exclusive_or.rb
@@ -1,12 +1,18 @@
-def xor(x, y)
-  !!x ^ !!y
+#def xor(x, y)
+  #!!x ^ !!y
+#end
+
+def xor?(value1, value2)
+  return true if value1 && !value2
+  return true if value2 && !value1
+  false
 end
 
-p xor(5.even?, 4.even?) == true
-p xor(5.odd?, 4.odd?) == true
-p xor(5.odd?, 4.even?) == false
-p xor(5.even?, 4.odd?) == false
-p xor('abc', nil) == true
-p xor(nil, 'abc') == true
-p xor('abc', 'abc') == false
-p xor(nil, nil) == false
+p xor?(5.even?, 4.even?) == true
+p xor?(5.odd?, 4.odd?) == true
+p xor?(5.odd?, 4.even?) == false
+p xor?(5.even?, 4.odd?) == false
+p xor?('abc', nil) == true
+p xor?(nil, 'abc') == true
+p xor?('abc', 'abc') == false
+p xor?(nil, nil) == false
