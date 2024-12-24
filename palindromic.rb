a59665857c56a2fd31d9dca7e69bb9b6d1f84f8e
Marcus Mosley <marcus.mosley1993@gmail.com>
2024-12-24 16:12:11 -0600

    palindromic.rb

--- palindromic.rb
+++ palindromic.rb
@@ -1,9 +1,14 @@
 def palindrome?(string)
-  string = string.reverse
+  string == string.reverse
 end
+
 def real_palindrome?(string)
   string = string.downcase.delete('^a-z0-9')
   palindrome?(string)
+end
+
+def palindromic_number?(number)
+  palindrome?(number.to_s)
 end
 
 p real_palindrome?('madam') == true
@@ -12,3 +17,7 @@ p real_palindrome?("Madam i'm Adam") == true
 p real_palindrome?('356653') == true
 p real_palindrome?('356a653') == true
 p real_palindrome?('123ab321') == false
+p palindromic_number?(34543) == true
+p palindromic_number?(123210) == false
+p palindromic_number?(22) == true
+p palindromic_number?(5) == true
