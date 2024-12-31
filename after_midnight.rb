e92d8c71e53d30f7bad4cceaae4d3b3770bec5b4
Marcus Mosley <marcus.mosley1993@gmail.com>
2024-12-31 15:16:31 -0600

    after_midnight.r

---
+++ after_midnight.rb
@@ -0,0 +1,43 @@
+MINUTES_PER_HOUR = 60
+HOURS_PER_DAY = 24
+MINUTES_PER_DAY = HOURS_PER_DAY * MINUTES_PER_HOUR
+
+def normalize_minutes_to_0_through_1439(minutes)
+  while minutes < 0
+    minutes += MINUTES_PER_DAY
+  end
+
+  minutes % MINUTES_PER_DAY
+end
+
+def time_of_day(delta_minutes)
+  delta_minutes = normalize_minutes_to_0_through_1439(delta_minutes)
+  hours, minutes = delta_minutes.divmod(MINUTES_PER_HOUR)
+  format('%02d:%02d', hours, minutes)
+end
+
+p time_of_day(0) == "00:00"
+p time_of_day(-3) == "23:57"
+p time_of_day(35) == "00:35"
+p time_of_day(-1437) == "00:03"
+p time_of_day(3000) == "02:00"
+p time_of_day(800) == "13:20"
+p time_of_day(-4231) == "01:29"
+
+def after_midnight(time_str)
+  hours, minutes = time_str.split(':').map(&:to_i)
+  (hours * MINUTES_PER_HOUR + minutes) % MINUTES_PER_DAY
+end
+
+def before_midnight(time_str)
+  delta_minutes = MINUTES_PER_DAY - after_midnight(time_str)
+  delta_minutes = 0 if delta_minutes == MINUTES_PER_DAY
+  delta_minutes
+end
+
+p after_midnight('00:00') == 0
+p before_midnight('00:00') == 0
+p after_midnight('12:34') == 754
+p before_midnight('12:34') == 686
+p after_midnight('24:00') == 0
+p before_midnight('24:00') == 0
